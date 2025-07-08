26689da2a6f8b8edfe94604194eb70725af7a500
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2025-07-08 14:19:08 +0330

    index done

--- app/Http/Resources/Api/V1/Product/ProductListResouce.php
+++ app/Http/Resources/Api/V1/Product/ProductListResouce.php
@@ -26,6 +26,7 @@ class ProductListResouce extends JsonResource
             'slug' => $this->slug,
             'price' => number_format($this->price),
             'price_without_discount' => number_format($this->price_without_discount),
+            'discount_percentage' => $this->discount_percentage,
             'snapp_pay_each_installment' => number_format($this->price/4)
         ];
     }
