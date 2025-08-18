12c3e025b0f5f9f43587e2180f6156e2ac749641
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2025-08-18 17:41:12 +0330

    update order user name

--- app/Http/Resources/Admin/Order/OrderItemResource.php
+++ app/Http/Resources/Admin/Order/OrderItemResource.php
@@ -15,6 +15,7 @@ class OrderItemResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'orderColumn' => $this->orderColumn,
             'persianStatus' => $this->persianStatus,
             'firstImageOfOrderItem' => $this->firstImageOfOrderItem,
