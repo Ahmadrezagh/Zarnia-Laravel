5acbaad379b9c8d7996a39d899fc76592291cf57
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2025-09-08 16:14:21 +0330

    print

--- resources/views/pdf/order.blade.php
+++ resources/views/pdf/order.blade.php
@@ -13,7 +13,6 @@
             width: 800px;
             height: 1120px; /* A4 تقریبی */
             margin: auto;
-            padding-left: 200px;
         }
         #pdf-canvas, #bg-image {
             width: 100%;
