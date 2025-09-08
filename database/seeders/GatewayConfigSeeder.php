34e84ef35cd96d6b22d3e4e199b1e4570ccd5c79
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2025-09-08 19:16:55 +0330

    update thank you

--- database/seeders/GatewayConfigSeeder.php
+++ database/seeders/GatewayConfigSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Gateway;
 use Illuminate\Database\Seeder;
 use App\Models\GatewayConfig;
 
@@ -12,64 +13,66 @@ class GatewayConfigSeeder extends Seeder
      */
     public function run(): void
     {
+        $snapp = Gateway::query()->where('title','=','قسطی')->first();
+        $saman = Gateway::query()->where('title','=','نقدی')->first();
         $configs = [
             // ================= Saman =================
             [
                 'key'        => 'saman_merchant_id',
                 'value'      => '14631093',
-                'gateway_id' => 4,
+                'gateway_id' => $saman->id,
             ],
             [
                 'key'        => 'saman_redirect_base',
                 'value'      => 'https://sep.shaparak.ir/OnlinePG/SendToken',
-                'gateway_id' => 4,
+                'gateway_id' => $saman->id,
             ],
             [
                 'key'        => 'saman_init_endpoint',
                 'value'      => 'https://sep.shaparak.ir/OnlinePG/OnlinePG',
-                'gateway_id' => 4,
+                'gateway_id' => $saman->id,
             ],
             [
                 'key'        => 'saman_verify_endpoint',
                 'value'      => 'https://sep.shaparak.ir/OnlinePG/VerifyToken',
-                'gateway_id' => 4,
+                'gateway_id' => $saman->id,
             ],
             [
                 'key'        => 'saman_timeout',
                 'value'      => '20',
-                'gateway_id' => 4,
+                'gateway_id' => $saman->id,
             ],
             [
                 'key'        => 'saman_sandbox',
                 'value'      => '0',
-                'gateway_id' => 4,
+                'gateway_id' => $saman->id,
             ],
 
             // ================= SnappPay =================
             [
                 'key'        => 'SNAPPPAY_USERNAME',
                 'value'      => 'your-username',
-                'gateway_id' => 3,
+                'gateway_id' => $snapp->id,
             ],
             [
                 'key'        => 'SNAPPPAY_PASSWORD',
                 'value'      => 'your-password',
-                'gateway_id' => 3,
+                'gateway_id' => $snapp->id,
             ],
             [
                 'key'        => 'SNAPPPAY_CLIENT_ID',
                 'value'      => 'your-client-id',
-                'gateway_id' => 3,
+                'gateway_id' => $snapp->id,
             ],
             [
                 'key'        => 'SNAPPPAY_CLIENT_SECRET',
                 'value'      => 'your-client-secret',
-                'gateway_id' => 3,
+                'gateway_id' => $snapp->id,
             ],
             [
                 'key'        => 'SNAPPPAY_BASE_URL',
                 'value'      => 'https://example.snappay.ir',
-                'gateway_id' => 3,
+                'gateway_id' => $snapp->id,
             ],
         ];
 
