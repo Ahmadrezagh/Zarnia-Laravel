0c31c0c06ceee35dd2a4914f1b3b99ef13d9442d
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2025-09-22 13:36:53 +0330

    slide bigger

--- app/Http/Controllers/Api/V1/InitController.php
+++ app/Http/Controllers/Api/V1/InitController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use App\Models\Setting;
 use App\Models\SettingGroup;
 use Illuminate\Http\Request;
@@ -14,6 +15,7 @@ class InitController extends Controller
         $footer_setting_group = SettingGroup::query()->where('title', 'فوتر')->first();
         $footer = [];
         $footer_settings = Setting::query()->where('setting_group_id', $footer_setting_group->id)->get();
+        $gold_price = Product::query()->whereNotNull('mazaneh')->first() ? Product::query()->whereNotNull('mazaneh')->first()->mazaneh : 0;
         foreach ($footer_settings as $setting) {
             $footer[$setting->key] = $setting->value;
         }
@@ -22,6 +24,7 @@ class InitController extends Controller
             'telegram' => setting('telegram'),
             'whatsapp' => setting('whatsapp'),
             'instagram' => setting('instagram'),
+            'gold_price' => $gold_price,
             'footer' => $footer
         ]);
     }
