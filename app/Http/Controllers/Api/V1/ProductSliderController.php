abd8cad4e88e633df5223b7b920709612767540d
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2025-10-04 18:49:01 +0330

    cart and checkout

--- app/Http/Controllers/Api/V1/ProductSliderController.php
+++ app/Http/Controllers/Api/V1/ProductSliderController.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\PorductSlider\ProductSliderResource;
+use App\Models\Product;
 use App\Models\ProductSlider;
 use Illuminate\Http\Request;
 
 class ProductSliderController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $user = $request->user('sanctum');
+        $products = Product::query()
+            ->main()
+            ->OrderByEffectivePrice($request->price_dir)
+            ->categories($request->category_ids)
+            ->search($request->search)
+            ->minPrice($request->minPrice)
+            ->maxPrice($request->maxPrice)
+            ->HasDiscount($request->hasDiscount)
+            ->hasCountAndImage()
+            ->paginate($request->get('per_page') ?? 12);
         return ProductSliderResource::collection(ProductSlider::all());
     }
 }
