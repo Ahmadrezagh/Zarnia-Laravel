bf43ac93268f33e20acf07303834c86a687f99b2
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2025-10-05 14:02:41 +0330

    date picker bug fix

--- resources/views/pdf/order.blade.php
+++ resources/views/pdf/order.blade.php
@@ -22,7 +22,7 @@
         body {
             margin: 0;
             padding: 0;
-            font-family: 'IRANSans', sans-serif;
+            font-family: 'IRANSans';
         }
         #editor-container {
             position: relative;
