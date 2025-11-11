b34b8d632ccb62841a098913b195771a43a0718b
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2025-11-11 17:52:18 +0330

    front bug fix

--- resources/views/thank-you/index.blade.php
+++ resources/views/thank-you/index.blade.php
@@ -5,6 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>تشکر از خرید - زرنیا</title>
     <link rel="stylesheet" href="{{ asset('thank-you/styles.css') }}">
+    <script>
+    !function (t, e, n) {
+        t.yektanetAnalyticsObject = n, t[n] = t[n] || function () {
+            t[n].q.push(arguments)
+        }, t[n].q = t[n].q || [];
+        var a = new Date, r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(),
+            c = e.getElementsByTagName("script")[0], s = e.createElement("script");
+        s.id = "ua-script-8ByW8wQG"; s.dataset.analyticsobject = n;
+        s.async = 1; s.type = "text/javascript";
+        s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/8ByW8wQG/rg.complete.js?v=" + r, c.parentNode.insertBefore(s, c)
+    }(window, document, "yektanet");
+</script>
 </head>
 <body>
 <div class="thank-you-container">
