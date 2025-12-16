c1d17a5e6c03169d785bb88a38b9f32234e520d5
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2025-12-16 17:04:45 +0330

    categories test

--- app/Http/Resources/Admin/Table/AdminProductResource.php
+++ app/Http/Resources/Admin/Table/AdminProductResource.php
@@ -23,7 +23,7 @@ class AdminProductResource extends JsonResource
             'ojrat' => $this->ojrat,
             'darsad_kharid' => $this->darsad_kharid,
             'price' => number_format($this->price)." تومان",
-//            'categories_title' => $this->categories_title,
+            'categories_title' => $this->categories_title,
 //            'categories_title_truncated' => $this->getTruncatedCategoriesTitle(30),
 //            'discount_percentage' => $this->discount_percentage,
 //            'count' => $this->count,
