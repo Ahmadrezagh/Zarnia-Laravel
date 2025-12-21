026b2ff8292af63dca9136dbc7d7451b5405bee4
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2025-12-21 18:04:22 +0330

    mobile icon

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -220,6 +220,7 @@ class OrderController extends Controller
                 'transaction_id' => $transactionId,
                 'note' => $note,
                 'paid_at' => in_array($request->status, ['paid', 'boxing', 'sent', 'post', 'completed']) ? now() : null,
+                'gold_price' => number_format(get_gold_price()/10)
             ]);
 
             // Create order items
