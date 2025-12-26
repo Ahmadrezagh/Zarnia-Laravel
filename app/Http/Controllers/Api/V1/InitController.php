f664678fa065bf309185cb102aeaa1aab205c06b
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2025-12-26 18:56:31 +0330

    etiket based shopping cart

--- app/Http/Controllers/Api/V1/InitController.php
+++ app/Http/Controllers/Api/V1/InitController.php
@@ -7,6 +7,7 @@ use App\Models\Product;
 use App\Models\Setting;
 use App\Models\SettingGroup;
 use Illuminate\Http\Request;
+use Morilog\Jalali\Jalalian;
 
 class InitController extends Controller
 {
@@ -19,13 +20,39 @@ class InitController extends Controller
         foreach ($footer_settings as $setting) {
             $footer[$setting->key] = $setting->value;
         }
+
+        // Generate next 7 days with both Gregorian and Jalali dates
+        $persian_day_names = [
+            'یکشنبه',
+            'دوشنبه',
+            'سه‌شنبه',
+            'چهارشنبه',
+            'پنج‌شنبه',
+            'جمعه',
+            'شنبه'
+        ];
+        
+        $next_seven_days = [];
+        for ($i = 0; $i < 7; $i++) {
+            $date = now()->addDays($i);
+            $jalali = Jalalian::forge($date);
+            $day_of_week = $date->dayOfWeek; // 0 = Sunday, 6 = Saturday
+            
+            $next_seven_days[] = [
+                'gregorian' => $date->format('Y-m-d'),
+                'jalali' => $jalali->format('Y-m-d'),
+                'day_name' => $jalali->format('D'),
+            ];
+        }
+
         return response()->json([
             'name' => setting('name'),
             'telegram' => setting('telegram'),
             'whatsapp' => setting('whatsapp'),
             'instagram' => setting('instagram'),
             'gold_price' => number_format($gold_price/10),
-            'footer' => $footer
+            'footer' => $footer,
+            'next_seven_days' => $next_seven_days
         ]);
     }
 }
