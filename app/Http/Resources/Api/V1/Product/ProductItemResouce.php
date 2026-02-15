f0d916841ce2d3b9495ce010dfae54596e60e490
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2026-02-15 13:44:19 +0330

    update files use pridcts price

--- app/Http/Resources/Api/V1/Product/ProductItemResouce.php
+++ app/Http/Resources/Api/V1/Product/ProductItemResouce.php
@@ -70,7 +70,7 @@ class ProductItemResouce extends JsonResource
             ),
             'slug' => $this->slug,
             'price' => number_format($this->minimum_available_price),
-            'price_without_discount' => number_format($this->price_without_discount),
+            'price_without_discount' => number_format($this->price_without_discount_minimum_available_product * 10 ?? 0),
             'price_range_title' => $this->price_range_title,
             'minimum_available_price' => $this->minimum_available_price,
             'minimum_available_weight' => $this->minimum_available_weight,
