3564b77b2b5464d4402b655cd854f5d09300e325
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2026-02-15 13:11:25 +0330

    update files use pridcts price

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -780,7 +780,7 @@ class Product extends Model implements HasMedia
             ->min('price');
         
         if ($lowestEtiketPrice) {
-            return $lowestEtiketPrice / 10;
+            return $lowestEtiketPrice ;
         }
         
         // If no available etikets, check children's etikets
@@ -792,7 +792,7 @@ class Product extends Model implements HasMedia
                 ->min('etikets.price');
             
             if ($lowestChildEtiketPrice) {
-                return $lowestChildEtiketPrice / 10;
+                return $lowestChildEtiketPrice ;
             }
         }
         
