10cd9e8f8429991128716f19d5012bc638330bdc
Ahmadreza <ahmadreza@Ahmadrezas-MacBook-Pro.local>
2026-05-07 11:50:46 +0330

    user name

--- app/Models/User.php
+++ app/Models/User.php
@@ -189,7 +189,7 @@ class User extends Authenticatable
         }
         
         return $this->addresses->map(function($address) {
-            return "({$user->name})- {$address->address}";
+            return "({$address->user->name})- {$address->address}";
         })->implode("\n");
     }
 
